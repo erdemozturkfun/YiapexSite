908c589a22b10ab9477a037d54ccbf2d51d1b25e
Erdogames <erdemozturk200@gmail.com>
2024-10-16 20:17:58 +0300

    heeys

--- components/AppNavBar.vue
+++ components/AppNavBar.vue
@@ -25,7 +25,7 @@ onMounted(() => {
           <div class="flex  content-center w-full items-center bg-red-800 h-full transition justify-center" :class="{'bg-transparent':transStore.transparency, 'translate-y-2': transStore.transparency}">
             <div class="flex">
             <div class="rounded-full hover:bg-zinc-900 w-16 h-16 flex justify-center items-center transition" :class="{'bg-zinc-900':!transStore.transparency}">
-            <img class="h-12 xl:w-12 bg-transparent    transition " src="/yiapex logo.svg"/>
+            <img class="h-12 xl:w-12 bg-transparent    transition " src="/yiapexlogo.svg"/>
           </div>
            <div class="flex flex-row items-center md:ml-96  shadow-2xl gap-x-10 outfit-links rounded-full bg-white px-4">
             <NuxtLink to="/" class=" transition hover:scale-110">Ana Sayfa</NuxtLink> <NuxtLink to="/about" class=" transition hover:scale-110">Hakkımızda</NuxtLink> <NuxtLink to="/team" class=" transition hover:scale-110">Ekibimiz</NuxtLink> <h1>İletişim</h1></div>
